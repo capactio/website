85d627b96f5e06fd89f6f9af1beebcada73c8e70
Paweł Kosiec <pawel.kosiec@gmail.com>
2021-06-11 17:52:59 +0200

    Update branding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,8 +46,7 @@ const HomepageHeader = () => {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">A simple way to manage<br/>applications and infrastructure</h1>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
