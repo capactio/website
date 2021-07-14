7c8731b3450b1865579a1b210b60057b92e8a878
Paweł Kosiec <pawel@structsure.co>
2021-07-14 12:10:24 +0200

    Add animated CLI commands preview on homepage (#31)

---
+++ src/components/homepage/GetStartedTerminal.tsx
@@ -0,0 +1,45 @@
+import React, { FunctionComponent } from "react";
+import 'react-animated-term/dist/react-animated-term.css';
+import "./GetStartedTerminal.overrides.css";
+import Terminal from 'react-animated-term';
+
+
+const spinner = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
+const cmdStep = (title: string) => ({
+  text: title,
+  cmd: true,
+  delay: 10
+})
+
+const spinningStep = (title: string, repeatCount = 0, frameDelay = 10) => ({
+  text: `✔ ${title}`,
+  cmd: false,
+  repeat: repeatCount > 0,
+  repeatCount: repeatCount,
+  frames: spinner.map((spinnerItem) => ({
+    text: `${spinnerItem} ${title}...`,
+    delay: frameDelay
+  }))
+})
+
+const termLines = [
+  cmdStep("capact env create kind"),
+  spinningStep("Creating cluster `kind-dev-capact`"),
+  cmdStep("capact install"),
+  spinningStep("Installing Capact on cluster"),
+  cmdStep("capact login https://gateway.capact.local -u user -p pass"),
+  {
+    text: '✔ Login succeeded',
+    delay: 10
+  },
+  cmdStep("# You're all set! Enjoy using Capact!"),
+]
+
+export const GetStartedTerminal: FunctionComponent = () => {
+  return (
+    <Terminal
+      lines={termLines}
+      interval={80}
+    />
+  )
+}
