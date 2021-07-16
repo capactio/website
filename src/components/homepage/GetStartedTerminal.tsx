31e3233ed2b3429b7271c2dee685bd85d8259cfa
Paweł Kosiec <pawel@capact.io>
2021-07-16 12:00:31 +0200

    Replace terminal with static one

--- src/components/homepage/GetStartedTerminal.tsx
+++ src/components/homepage/GetStartedTerminal.tsx
@@ -1,29 +1,42 @@
+import clsx from "clsx";
 import React, { FunctionComponent } from "react";
-import 'react-animated-term/dist/react-animated-term.css';
-import "./GetStartedTerminal.overrides.css";
-import Terminal from 'react-animated-term';
+import styles from "./GetStartedTerminal.module.css";
 
-const cmdStep = (title: string) => ({
-  text: title,
-  cmd: true,
-  delay: 10
-})
 
-const termLines = [
-  cmdStep("# Create local kind cluster"),
-  cmdStep("capact env create kind"),
-  cmdStep("# Install Capact"),
-  cmdStep("capact install"),
-  cmdStep("# Log in to the Capact Gateway"),
-  cmdStep("capact login https://gateway.capact.local -u user -p pass"),
-  cmdStep("# You're all set! Enjoy using Capact!"),
+const terminalData = [
+  {
+    text: "# Create local kind cluster",
+    dimmed: true,
+  },
+  {
+    text: "capact env create kind",
+  },
+  {
+    text: "# Install Capact",
+    dimmed: true,
+  },
+  {
+    text: "capact install",
+  },
+  {
+    text: "# Log in to the Capact Gateway",
+    dimmed: true,
+  },
+  {
+    text: "capact login https://gateway.capact.local -u user -p pass"
+  },
+  {
+    text: "# You're all set! Enjoy using Capact!",
+    dimmed: true
+  }
 ]
 
 export const GetStartedTerminal: FunctionComponent = () => {
   return (
-    <Terminal
-      lines={termLines}
-      interval={50}
-    />
+    <div className={styles.terminal} id="termynal" data-termynal>
+      {terminalData.map(({ text, dimmed }, idx) => (
+        <span key={idx} data-ty="input" className={clsx({ [styles.lineDimmed]: dimmed })}>{text}</span>
+      ))}
+    </div>
   )
 }
