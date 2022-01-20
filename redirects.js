711689f8865026c57561f824ff36428e3bf14352
Pawel Kosiec <pawel@capact.io>
2022-01-20 21:13:30 +0100

    Refactor redirects

--- redirects.js
+++ redirects.js
@@ -4,91 +4,132 @@
 // https://docusaurus.io/docs/api/plugins/@docusaurus/plugin-client-redirects
 
 module.exports = () => {
-  const redirects = [
-    ...generateLegacy05DocsRedirectsForVersion(""), // latest version
+  return [
+    ...generateDocsRedirectsForVersion(""), // latest version
     ...generateDocsRedirectsForVersion("next"), // unreleased version
-    ...generateLegacy04DocsRedirectsForVersion("0.4"),
-    ...generateLegacy05DocsRedirectsForVersion("0.5", true), // redirect from x.y version to latest
+    ...generateDocsRedirectsForVersion("0.4"),
+    ...generateDocsRedirectsForVersion("0.5"),
   ];
-
-  return redirects;
 };
 
-const generateDocsRedirectsForVersion = (version, useLatestVersionAsTarget) => {
-  const fromPrefix = version != "" ? `/docs/${version}` : "/docs";
-  const toPrefix = useLatestVersionAsTarget ? `/docs` : fromPrefix;
+const latestVersion = "0.5";
 
-  return [
+const generateDocsRedirectsForVersion = (version) => {
+  const fromPrefix = version != "" ? `/docs/${version}` : "/docs";
+
+  let toPrefix = fromPrefix;
+  if (version === latestVersion) {
+    // for latest version redirect to URL without version number
+    toPrefix = "/docs";
+  }
+
+  const mapping = [
     {
       from: `${fromPrefix}`,
       to: `${toPrefix}/getting-started`,
+      versions: {
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/installation`,
       to: `${toPrefix}/installation/local`,
+      versions: {
+        0.4: true,
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/example`,
       to: `${toPrefix}/example/mattermost-installation`,
+      versions: {
+        0.4: true,
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/content-development`,
       to: `${toPrefix}/content-development/guide`,
+      versions: {
+        0.4: true,
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/feature`,
       to: `${toPrefix}/feature/policies/overview`,
+      versions: {
+        0.4: true,
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/architecture`,
       to: `${toPrefix}/architecture/e2e-architecture`,
+      versions: {
+        0.4: true,
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/cli/commands`,
       to: `${toPrefix}/cli/commands/capact`,
+      versions: {
+        0.4: true,
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/operation`,
       to: `${toPrefix}/operation/common-problems`,
+      versions: {
+        0.4: true,
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/cli`,
       to: `${toPrefix}/cli/getting-started`,
+      versions: {
+        0.4: true,
+        0.5: true,
+        next: true,
+      },
     },
     {
       from: `${fromPrefix}/dashboard-ui`,
       to: `${toPrefix}/dashboard-ui/overview`,
+      versions: {
+        next: true,
+      },
     },
-  ];
-};
-
-const generateLegacy04DocsRedirectsForVersion = (version, useLatestVersionAsTarget) => {
-  const fromPrefix = version != "" ? `/docs/${version}` : "/docs";
-  const toPrefix = useLatestVersionAsTarget ? `/docs` : fromPrefix;
-
-	const currentRedirect = generateDocsRedirectsForVersion(version, useLatestVersionAsTarget).filter( item => {
-		return item.to !== `${toPrefix}/getting-started` && item.to !== `${toPrefix}/dashboard-ui/overview`;
-	})
-
-  return [
-    ...currentRedirect,
+    // legacy redirects
     {
       from: `${fromPrefix}`,
       to: `${toPrefix}/introduction`,
+      versions: {
+        0.4: true,
+      },
     },
     {
       from: `${fromPrefix}/development`,
       to: `${toPrefix}/development/development-guide`,
+      versions: {
+        0.4: true,
+      },
     },
   ];
-};
 
-
-const generateLegacy05DocsRedirectsForVersion = (version, useLatestVersionAsTarget) => {
-  const fromPrefix = version != "" ? `/docs/${version}` : "/docs";
-  const toPrefix = useLatestVersionAsTarget ? `/docs` : fromPrefix;
-
-	return generateDocsRedirectsForVersion(version, useLatestVersionAsTarget).filter( item => {
-		return item.to !== `${toPrefix}/dashboard-ui/overview`;
-	})
+  const versionToCheck = version != "" ? version : latestVersion;
+  return mapping
+    .filter((item) => item.versions[versionToCheck])
+    .map(({from, to}) => ({from, to}));
 };
