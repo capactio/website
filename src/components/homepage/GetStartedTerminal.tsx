1fc6bbfe06e3729f8747a39a8dd1636e317e2aca
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-01-27 09:50:39 +0100

    Apply suggestions after review

--- src/components/homepage/GetStartedTerminal.tsx
+++ src/components/homepage/GetStartedTerminal.tsx
@@ -1,37 +1,9 @@
 import clsx from "clsx";
-import React, { FunctionComponent } from "react";
+import React, {FunctionComponent} from "react";
 import styles from "./GetStartedTerminal.module.css";
 
 
 const stableTerminalData = [
-  {
-    text: "# Create local kind cluster",
-    dimmed: true,
-  },
-  {
-    text: "capact env create kind",
-  },
-  {
-    text: "# Install Capact",
-    dimmed: true,
-  },
-  {
-    text: "capact install",
-  },
-  {
-    text: "# Log in interactively to the Capact Gateway",
-    dimmed: true,
-  },
-  {
-    text: "capact login https://gateway.capact.local"
-  },
-  {
-    text: "# You're all set! Enjoy using Capact!",
-    dimmed: true
-  }
-]
-
-const latestTerminalData = [
 	{
 		text: "# Create local k3d cluster",
 		dimmed: true,
@@ -44,7 +16,7 @@ const latestTerminalData = [
 		dimmed: true,
 	},
 	{
-		text: "capact install --version @latest --helm-repo @latest",
+		text: "capact install",
 	},
 	{
 		text: "# Log in interactively to the Capact Gateway",
@@ -59,17 +31,12 @@ const latestTerminalData = [
 	}
 ]
 
-interface GetStartedTerminalInput {
-	latest?: boolean
-}
-
-export const GetStartedTerminal: FunctionComponent<GetStartedTerminalInput> = ({latest}: GetStartedTerminalInput) => {
-	const steps = latest ? latestTerminalData : stableTerminalData
-  return (
-    <div className={styles.terminal} id="termynal" data-termynal>
-      {steps.map(({ text, dimmed }, idx) => (
-        <span key={idx} data-ty="input" className={clsx({ [styles.lineDimmed]: dimmed })}>{text}</span>
-      ))}
-    </div>
-  )
+export const GetStartedTerminal: FunctionComponent = () => {
+	return (
+		<div className={styles.terminal} id="termynal" data-termynal>
+			{stableTerminalData.map(({text, dimmed}, idx) => (
+				<span key={idx} data-ty="input" className={clsx({[styles.lineDimmed]: dimmed})}>{text}</span>
+			))}
+		</div>
+	)
 }
