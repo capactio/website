c2ca80338bac38325a7300d816aaf67be15c80fe
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-02-04 09:07:42 +0100

    Add "Take Capact for a spin!" blog post (#99)

--- src/components/homepage/GetStartedTerminal.tsx
+++ src/components/homepage/GetStartedTerminal.tsx
@@ -1,42 +1,42 @@
 import clsx from "clsx";
-import React, { FunctionComponent } from "react";
+import React, {FunctionComponent} from "react";
 import styles from "./GetStartedTerminal.module.css";
 
 
-const terminalData = [
-  {
-    text: "# Create local kind cluster",
-    dimmed: true,
-  },
-  {
-    text: "capact env create kind",
-  },
-  {
-    text: "# Install Capact",
-    dimmed: true,
-  },
-  {
-    text: "capact install",
-  },
-  {
-    text: "# Log in interactively to the Capact Gateway",
-    dimmed: true,
-  },
-  {
-    text: "capact login https://gateway.capact.local"
-  },
-  {
-    text: "# You're all set! Enjoy using Capact!",
-    dimmed: true
-  }
+const stableTerminalData = [
+	{
+		text: "# Create local k3d cluster",
+		dimmed: true,
+	},
+	{
+		text: "capact env create k3d",
+	},
+	{
+		text: "# Install Capact",
+		dimmed: true,
+	},
+	{
+		text: "capact install",
+	},
+	{
+		text: "# Log in interactively to the Capact Gateway",
+		dimmed: true,
+	},
+	{
+		text: "capact login https://gateway.capact.local"
+	},
+	{
+		text: "# You're all set! Enjoy using Capact!",
+		dimmed: true
+	}
 ]
 
 export const GetStartedTerminal: FunctionComponent = () => {
-  return (
-    <div className={styles.terminal} id="termynal" data-termynal>
-      {terminalData.map(({ text, dimmed }, idx) => (
-        <span key={idx} data-ty="input" className={clsx({ [styles.lineDimmed]: dimmed })}>{text}</span>
-      ))}
-    </div>
-  )
+	return (
+		<div className={styles.terminal} id="termynal" data-termynal>
+			{stableTerminalData.map(({text, dimmed}, idx) => (
+				<span key={idx} data-ty="input" className={clsx({[styles.lineDimmed]: dimmed})}>{text}</span>
+			))}
+		</div>
+	)
 }
